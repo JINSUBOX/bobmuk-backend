ec2e64383cb5aeb4c72722c292ffaffb7e939ac5
JINSUBOX <jinsubox@gmail.com>
2023-12-09 17:25:28 +0900

    init

---
+++ src/main/java/com/jinsubox/bobmukbackend/BobmukBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jinsubox.bobmukbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobmukBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BobmukBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jinsubox/bobmukbackend/BobmukBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinsubox.bobmukbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobmukBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
